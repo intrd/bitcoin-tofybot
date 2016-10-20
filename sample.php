9ce50e350850a542722447270a44d65c741dae7a
intrd <x@dann.com.br>
2016-10-20 10:07:46 -0200

    .

--- sample.php
+++ sample.php
@@ -27,12 +27,14 @@ $conf = parse_ini_file("config.ini", false);
 
 date_default_timezone_set($conf["timezone"]); 
 $conf["root"]=dirname(__FILE__)."/";
-i::check_dir(array($conf["root"]."DATA/",$conf["root"]."TMP/",$conf["root"]."LOGS/")); 
-$ext_path=$conf["root"]."../";
-$conf["tmp_path"]=$conf["root"]."TMP/";
-$conf["data_path"]=$conf["root"]."DATA/";
-$cookie=$conf["tmp_path"]."cookie"; 
-if (!file_exists($conf["data_path"]."secrets.ini")) die("\n*** config.ini does no exist.\n");
+$conf["ext_path"]=$conf["root"].$conf["ext"];
+$conf["tmp_path"]=$conf["root"].$conf["tmp"];
+$conf["data_path"]=$conf["root"].$conf["data"];
+$conf["logs_path"]=$conf["root"].$conf["logs"];
+i::check_dir(array($conf["data_path"],$conf["tmp_path"],$conf["logs_path"])); 
+$conf["cookie1"]=$conf["tmp_path"].$conf["cookie1"];
+
+if (!file_exists($conf["data_path"]."secrets.ini")) die("\n*** secrets.ini does no exist.\n");
 $secrets = parse_ini_file($conf["data_path"]."secrets.ini", false);
 
 bot::hello(); //bot welcome screen
@@ -45,8 +47,8 @@ $client = new okc(new OKCoin_ApiKeyAuthentication($secrets["API_KEY"], $secrets[
 //$params = array('api_key' => $secrets["API_KEY"]);
 //$result = $client -> userinfoApi($params);
 
-//$params = array('symbol' => 'btc_usd', 'contract_type' => 'this_week');
-//$result = $client -> tickerFutureApi($params);
+$params = array('symbol' => 'btc_usd', 'contract_type' => 'this_week');
+$result = $client -> tickerFutureApi($params);
 
 // $params = array('symbol' => 'btc_usd', 'contract_type' => 'this_week');
 // $result = $client -> tradesFutureApi($params);
@@ -57,7 +59,7 @@ $client = new okc(new OKCoin_ApiKeyAuthentication($secrets["API_KEY"], $secrets[
 // $params = array('api_key' => $secrets["API_KEY"], 'symbol' => 'btc_usd', 'contract_type' => 'this_week');
 // $result = $client -> positionFutureApi($params);
 
-$params = array('api_key' => $secrets["API_KEY"], 'symbol' => 'btc_usd', 'contract_type' => 'this_week', 'type' => 1);
-$result = $client -> singleBondPositionFutureApi($params);
+// $params = array('api_key' => $secrets["API_KEY"], 'symbol' => 'btc_usd', 'contract_type' => 'this_week', 'type' => 1);
+// $result = $client -> singleBondPositionFutureApi($params);
 
 print_r($result);
